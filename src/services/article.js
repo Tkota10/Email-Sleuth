a6969509905667cc2649a791534ef9fc3ef67bc9
Tkota10 <tkota0910@gmail.com>
2023-05-19 15:57:45 -0500

    Added API endpoints/ Currently Testing

--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,22 @@
-import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const rapidApiKey = import.meta.env.VITE_RAPID_API_EMAIL_KEY;
 
-export const articleApi = createApi ({
-    reducerPath: 'articleApi',
-    endpoints: (builder) =>  ({
-        getSummary: builder.query({
-            query: (params) => 'test'
-        })
-    })
+export const articleApi = createApi({
+  reducerPath: 'articleApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://email-scraper.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', rapidApiKey);
+      headers.set('X-RapidAPI-Host', 'email-scraper.p.rapidapi.com');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+        query: (params) => `email-extractor?url=${encodeURIComponent(params.articleUrl)}`,
+    }),
+  }),
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
